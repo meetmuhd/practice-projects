09d41874f2aed55c4f47e48ecd077458382eaf5a
Muhammad Bello <meetmuhd@gmail.com>
2019-06-12 16:51:29 +0100

    state game rules/functionality

---
+++ number-guesser/app.js
@@ -0,0 +1,13 @@
+/*************************************************************************
+GAME RULES/FUNCTIONALITY
+
+A. Player must guess a number between a min and a max.
+
+B. Player is given only a certan number of guessing attempts.
+
+C. Game is won when player guesses the number correctly within given attempts and gets a winning notification and option to play again.
+
+D. Game is lost if player fails to guess the correct number on their last guessing attempt and gets losing notification including correct answer and option to play again.
+
+E. Notify player of number of attempts remaining upon failed attempts before last one.
+*************************************************************************/
