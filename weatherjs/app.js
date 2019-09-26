01eb5bb35f839918340f6afe2fb9ea3503fd05ea
Muhammad Bello <meetmuhd@gmail.com>
2019-09-26 19:31:29 +0100

    make weather location flexible and persist to local storage

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,5 +1,9 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData()
 // Init weather object
-const weather = new Weather("Abuja", "NG");
+const weather = new Weather(weatherLocation.city, weatherLocation.countryCode);
 // Init UI
 const ui = new UI();
 
@@ -11,7 +15,11 @@ document.getElementById("w-change-btn").addEventListener("click", (e) => {
   const city = document.getElementById("city").value;
   const countryCode = document.getElementById("country-code").value;
 
+  // Change location
   weather.changeLocation(city, countryCode);
+
+  // Set location in Location Storage
+  storage.setLocationData(city, countryCode);
 
   // Get and display weather
   getWeather();
