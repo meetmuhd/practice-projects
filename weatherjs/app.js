16a7d75f5cb1fa858ad7d756aa0477d5863c1ffa
Muhammad Bello <meetmuhd@gmail.com>
2019-09-26 18:41:11 +0100

    implement change location modal functionality

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -6,6 +6,20 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+  const countryCode = document.getElementById("country-code").value;
+
+  weather.changeLocation(city, countryCode);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $("#locModal").modal("hide");
+});
+
 function getWeather() {
   weather.getWeather()
     .then(results => {
