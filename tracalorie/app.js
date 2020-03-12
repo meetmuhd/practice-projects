8690f6494856f37b6e5b80f2e94fc2f119973837
Muhammad A.G. Bello <magbello157@gmail.com>
2020-03-12 19:34:52 +0100

    implement edit state

--- tracalorie/app.js
+++ tracalorie/app.js
@@ -48,6 +48,26 @@ const ItemController = (function () {
       return newItem;
     },
 
+    getItemById: function (id) {
+      let found = null;
+
+      // Loop through items
+      data.items.forEach(item => {
+        if (item.id === id) {
+          found = item;
+        }
+      });
+      return found;
+    },
+
+    setCurrentItem: function (item) {
+      data.currentItem = item;
+    },
+
+    getCurrentItem: function () {
+      return data.currentItem;
+    },
+
     getTotalCalories: function () {
       let total = 0;
 
@@ -75,6 +95,9 @@ const UIController = (function () {
   const UISelectors = {
     itemList: "#item-list",
     addBtn: ".add-btn",
+    updateBtn: ".update-btn",
+    deleteBtn: ".delete-btn",
+    backBtn: ".back-btn",
     itemNameInput: "#item-name",
     itemCaloriesInput: "#item-calories",
     totalCalories: ".total-calories"
@@ -128,12 +151,33 @@ const UIController = (function () {
       document.querySelector(UISelectors.itemCaloriesInput).value = "";
     },
 
+    addItemToForm: function () {
+      document.querySelector(UISelectors.itemNameInput).value = ItemController.getCurrentItem().name;
+      document.querySelector(UISelectors.itemCaloriesInput).value = ItemController.getCurrentItem().calories;
+      UIController.showEditState();
+    },
+
     hideList: function () {
       document.querySelector(UISelectors.itemList).style.display = "none";
     },
 
     showTotalCalories: function (totalCalories) {
       document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
+
+    clearEditState: function () {
+      UIController.clearInput();
+      document.querySelector(UISelectors.updateBtn).style.display = "none";
+      document.querySelector(UISelectors.deleteBtn).style.display = "none";
+      document.querySelector(UISelectors.backBtn).style.display = "none";
+      document.querySelector(UISelectors.addBtn).style.display = "inline";
+    },
+
+    showEditState: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = "inline";
+      document.querySelector(UISelectors.deleteBtn).style.display = "inline";
+      document.querySelector(UISelectors.backBtn).style.display = "inline";
+      document.querySelector(UISelectors.addBtn).style.display = "none";
     },
 
     getSelectors: function () {
@@ -155,6 +199,9 @@ const AppController = (function (ItemController, UIController) {
 
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener("click", itemAddSubmit);
+
+    // Edit icon click event
+    document.querySelector(UISelectors.itemList).addEventListener("click", itemUpdateSubmit);
   }
 
   // Add item submit
@@ -184,9 +231,39 @@ const AppController = (function (ItemController, UIController) {
     e.preventDefault();
   }
 
+  // Update item submit
+  const itemUpdateSubmit = function (e) {
+    if (e.target.classList.contains("edit-item")) {
+      // Get list item id
+      const listId = e.target.parentNode.parentNode.id;
+
+      // Differentiate "item" from number
+      const listIdArr = listId.split("-");
+
+      // Get the actual ID
+      const id = parseInt(listIdArr[1]);
+
+      // Get item to edit
+      const itemToEdit = ItemController.getItemById(id);
+
+      // Set current item
+      ItemController.setCurrentItem(itemToEdit);
+
+      // Add item to form
+      UIController.addItemToForm();
+
+    }
+
+    e.preventDefault();
+  }
+
   // Public methods
   return {
     init: function () {
+
+      // Clear edit state / set initial state
+      UIController.clearEditState();
+
       // Fetch items from data structure
       const items = ItemController.getItems();
 
